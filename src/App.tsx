860f11c93332b56620d83de27f5b942bd0ece1a8
Charlie025x <charliealonso2001@gmail.com>
2024-06-01 02:58:38 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <>
+      <div>
+        <div className="flex bg-slate-500">
+          <h1>Todo</h1>
+        </div>
+        <div className="list"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
